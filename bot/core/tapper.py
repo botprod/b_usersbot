97115589a7af44809fcbd74bca804469732ed1bb
Gerfesto <hameilleb@gmail.com>
2024-08-29 01:35:24 +0300

    Update tapper.py

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -205,7 +205,7 @@ class Tapper:
     async def check_proxy(self, http_client: aiohttp.ClientSession) -> None:
         response = await self.make_request(http_client, 'GET', url='https://httpbin.org/ip', timeout=aiohttp.ClientTimeout(5))
         ip = response.get('origin')
-        logger.info(f"{self.session_name} | Proxy IP: <lc>{ip}<lc>")
+        logger.info(f"{self.session_name} | Proxy IP: <lc>{ip}</lc>")
         
         
     
